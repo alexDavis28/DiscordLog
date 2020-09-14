04b90cba06c44c0f88e7b4961241ccc039161558
Alex Davis <8m3xlt@gmail.com>
2020-09-14 23:25:12 +0100

    Working webhook system and log command

--- main.lua
+++ main.lua
@@ -1,52 +1,93 @@
+-- set plugin folder
+homeDir = 'Plugins/DiscordLog'
+
+-- Plugin name and prefix for later usage
+PluginName = "DiscordLog"
+PluginPrefix = PluginName .. ": "
+
+-- initialize global tables
+g_Config = {}
+
 function Initialize(Plugin)
-	Plugin:SetName("DerpyPluginThatBlowsPeopleUp")
-	Plugin:SetVersion(9001)
+	-- Start the plugin
+	Plugin:SetName("DiscordLog")
+	Plugin:SetVersion(0.2)
 
-	cPluginManager.BindCommand("/explode", "derpyplugin.explode", Explode, " ~ Explode a player");
+	-- Load the config
+	InitializeConfig()
 
-	cPluginManager:AddHook(cPluginManager.HOOK_COLLECTING_PICKUP, OnCollectingPickup)
+	-- Check if URL is set
+	if (g_Config.URL=="") then
+		error("Please set a webhook URL in DiscordLog.ini")
+	end
 
+	-- Add the test command
+	cPluginManager.BindCommand("/log", "DL.log", Log, " ~ Logs a message");
+
+	-- Log the plugin going online
 	LOG("Initialised " .. Plugin:GetName() .. " v." .. Plugin:GetVersion())
 	return true
 end
 
-function Explode(Split, Player)
-	if (#Split ~= 2) then
-		-- There was more or less than one argument (excluding the "/explode" bit)
-		-- Send the proper usage to the player and exit
-		Player:SendMessage("Usage: /explode [playername]")
+-- use a config.ini file
+function InitializeConfig()
+
+-- https://newsletter.cuberite.org/assets/cuberite.png
+
+    local g_ini = cIniFile()
+    iniFile = "DiscordLog.ini"
+    key = "Webhook"
+    g_ini:ReadFile(iniFile);
+    if not cFile:IsFile(iniFile) then
+        -- webhook settings
+        g_Config.URL  = g_ini:GetValueSet(key, "URL", "");
+        g_Config.PFP   = g_ini:GetValueSet(key, "PFP", "https://newsletter.cuberite.org/assets/cuberite.png");
+
+        g_ini:WriteFile(iniFile);
+	else
+		g_Config.URL = g_ini:GetValue("Webhook","URL", "")
+		g_Config.PFP = g_ini:GetValue("Webhook", "PFP", "https://newsletter.cuberite.org/assets/cuberite.png")
+    end
+
+end
+
+function Log(Split, Player)
+	if (#Split < 2) then
+		-- Confirm there is at least one argument
+		Player:SendMessage("Usage: /log [text]")
 		return true
 	end
 
-	-- Create a callback ExplodePlayer with parameter Explodee, which Cuberite calls for every player on the server
-	local HasExploded = false
-	local ExplodePlayer = function(Explodee)
-		-- If the player name matches exactly
-		if (Explodee:GetName() == Split[2]) then
-			-- Create an explosion of force level 2 at the same position as they are
-			-- see API docs for further details of this function
-			Player:GetWorld():DoExplosionAt(10, Explodee:GetPosX(), Explodee:GetPosY(), Explodee:GetPosZ(), false, esPlugin)
-			Player:SendMessageSuccess(Split[2] .. " was successfully exploded")
-			HasExploded = true;
-			return true -- Signalize to Cuberite that we do not need to call this callback for any more players
-		end
-	end
-
-	-- Tell Cuberite to loop through all players and call the callback above with the Player object it has found
-	cRoot:Get():FindAndDoWithPlayer(Split[2], ExplodePlayer)
-
-	if not(HasExploded) then
-		-- We have not broken out so far, therefore, the player must not exist, send failure
-		Player:SendMessageFailure(Split[2] .. " was not found")
-	end
-
+	send_webhook(Split[2], Split[3])
 	return true
 end
 
-function OnCollectingPickup(Player, Pickup) -- Again, see the API docs for parameters of all hooks. In this case, it is a Player and Pickup object
-	if (Player:GetClientHandle():GetPing() > 100) then -- Get ping of player, in milliseconds
-		return true -- Discriminate against high latency - you don't get drops :D
-	else
-		return false -- You do get the drops! Yay~
+function send_webhook(text, name)
+	-- Defualt name
+	name = name or "MINECRAFT SERVER"
+
+	-- Format the payload
+	local avatar = [["]] .. g_Config.PFP .. [["]]
+	local payload = [[ {"username":"]] .. name .. [[","avatar_url":]] .. avatar .. [[,"content":"]] .. text .. [["} ]]
+	-- Get the webhook url
+	local url  = g_Config.URL
+
+	local headers  = {["Content-Type"]="application/json"}
+
+	local callback  =  function (a_Body, a_Data)
+		if (a_Body) then
+			-- Response received correctly, a_Body contains the entire response body,
+			-- a_Data is a dictionary-table of the response's HTTP headers
+			if (a_Body==nil or a_Body=="") then
+				LOG("Webhook request successful, with message: '" ..text.."'")
+			else
+				LOG("Response from webhook request: " .. (a_Body) .. "")
+			end
+		else
+			error(a_Data)
+		end
 	end
+
+	-- Make the request
+	cUrlClient:Post(url, callback, headers, payload)
 end
